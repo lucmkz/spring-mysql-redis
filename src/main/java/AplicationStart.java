6de6ba105be3ce2601019d246816c0cedb012154
Lucas Duarte <l.duarte.mk@gmail.com>
2020-05-18 14:53:03 -0300

    util class date

--- src/main/java/AplicationStart.java
+++ src/main/java/AplicationStart.java
@@ -1,15 +1,16 @@
-package br.com.springMysql.start;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * Created by Lucas Duarte
  */
 
+//@SpringBootApplication
 @EnableAutoConfiguration
 @ComponentScan(basePackages = "br.com.springMysql.endpoint")
+@Configuration
 public class AplicationStart {
     public static void main(String[] args) {
         SpringApplication.run(AplicationStart.class, args);
---
+++ src/main/java/br/com/springMysql/util/DateUtil.java
@@ -0,0 +1,13 @@
+package br.com.springMysql.util;
+
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Component
+public class DateUtil {
+    public String formatLocalDateTimeToDataBaseStyle(LocalDateTime localDateTime) {
+        return DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").format(localDateTime);
+    }
+}

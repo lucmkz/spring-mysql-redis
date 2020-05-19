2564e9ac4a978549b5a269ce12b229932cef4c6f
Lucas Duarte <l.duarte.mk@gmail.com>
2020-05-19 17:41:26 -0300

    update and delete methods

--- src/main/java/br/com/springMysql/endpoint/ProductEndpoint.java
+++ src/main/java/br/com/springMysql/endpoint/ProductEndpoint.java
@@ -26,12 +26,12 @@ public class ProductEndpoint {
 //        this.dateUtil = dateUtil;
 //    }
 
-    @RequestMapping(method = RequestMethod.GET)
+    @GetMapping
     public ResponseEntity<?> listAll(){
         return new ResponseEntity<>(Product.productList, HttpStatus.OK);
     }
 
-    @RequestMapping(method = RequestMethod.GET, path = "/{id}")
+    @GetMapping(path = "/{id}")
     public ResponseEntity<?> getProductById(@PathVariable("id") int id){
         Product product = new Product();
         product.setId(id);
@@ -43,9 +43,21 @@ public class ProductEndpoint {
         return new ResponseEntity<>(Product.productList.get(index), HttpStatus.OK);
     }
 
-    @RequestMapping(method = RequestMethod.POST)
+    @PostMapping
     public ResponseEntity<?> save(@RequestBody Product product){
         Product.productList.add(product);
         return new ResponseEntity<>(product, HttpStatus.OK);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<?> delete(@RequestBody Product product){
+        Product.productList.remove(product);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PutMapping
+    public ResponseEntity<?> update(@RequestBody Product product){
+        Product.productList.add(product);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
